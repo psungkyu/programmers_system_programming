d81a3ad455995a285ff7393ef546dd488eb527b6
Seonggyu Park <prodinic13@gmail.com>
2023-05-15 20:43:49 +0900

    new repo for sp

---
+++ README.md
@@ -0,0 +1,1 @@
+# programmers system programming
---
+++ lab5_signal/toy/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <sys/wait.h>
+
+#include <system_server.h>
+#include <gui.h>
+#include <input.h>
+#include <web_server.h>
+
+/* 여기서 sigchld 핸들러 구현 */
+static void
+sigchldHandler(int sig)
+{
+    /* 구현 */
+    int status;
+    pid_t childPid;
+
+    printf("handler: Caught SigCHLD : %d\n", sig);
+    
+    while ((childPid = waitpid(-1, &status, WNOHANG)) > 0) {
+        printf("handler: Reaped child %ld - ", (long) childPid);
+        // printWaitStatus(NULL, status);
+        // numLiveChildren--;
+    }
+    
+    printf("handler : returning\n");
+}
+
+int main()
+{
+    pid_t spid, gpid, ipid, wpid;
+    int status, savedErrno;
+    int sigCnt;
+    sigset_t blockMask, emptyMask;
+    struct sigaction sa;
+
+    /* 여기서 SIGCHLD 시그널  등록 */
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = sigchldHandler;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        printf("sigaction!");
+        exit(0);
+    }
+
+    printf("메인 함수입니다.\n");
+    printf("시스템 서버를 생성합니다.\n");
+    spid = create_system_server();
+    printf("웹 서버를 생성합니다.\n");
+    wpid = create_web_server();
+    printf("입력 프로세스를 생성합니다.\n");
+    ipid = create_input();
+    printf("GUI를 생성합니다.\n");
+    gpid = create_gui();
+
+    waitpid(spid, &status, 0);
+    waitpid(gpid, &status, 0);
+    waitpid(ipid, &status, 0);
+    waitpid(wpid, &status, 0);
+
+    return 0;
+}

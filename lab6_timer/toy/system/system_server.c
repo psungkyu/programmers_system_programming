c123eab1ad3e8b1fe5c39938b907e2a2f65b994c
Seonggyu Park <prodinic13@gmail.com>
2023-05-16 14:32:16 +0900

    lab10 toy project completed

---
+++ lab6_timer/toy/system/system_server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <sys/prctl.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <time.h>
+
+#include <system_server.h>
+#include <gui.h>
+#include <input.h>
+#include <web_server.h>
+
+static int toy_timer = 0;
+
+int posix_sleep_ms(unsigned int timeout_ms)
+{
+    struct timespec sleep_time;
+
+    sleep_time.tv_sec = timeout_ms / MILLISEC_PER_SECOND;
+    sleep_time.tv_nsec = (timeout_ms % MILLISEC_PER_SECOND) * (NANOSEC_PER_USEC * USEC_PER_MILLISEC);
+
+    return nanosleep(&sleep_time, NULL);
+}
+
+int system_server()
+{
+    struct itimerspec ts;
+    struct sigaction  sa;
+    struct sigevent   sev;
+    timer_t *tidlist;
+
+    printf("나 system_server 프로세스!\n");
+
+    /* 5초 타이머를 만들어 봅시다. */
+
+    while (1) {
+        posix_sleep_ms(5000);
+    }
+
+    return 0;
+}
+
+int create_system_server()
+{
+    pid_t systemPid;
+    const char *name = "system_server";
+
+    printf("여기서 시스템 프로세스를 생성합니다.\n");
+
+    /* fork 를 이용하세요 */
+    switch (systemPid = fork()) {
+    case -1:
+        printf("fork failed\n");
+    case 0:
+        /* 프로세스 이름 변경 */
+        if (prctl(PR_SET_NAME, (unsigned long) name) < 0)
+            perror("prctl()");
+        system_server();
+        break;
+    default:
+        break;
+    }
+
+    return 0;
+}
